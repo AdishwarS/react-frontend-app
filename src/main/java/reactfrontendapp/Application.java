f59e54bdcd4f62d62a2d13fba64b842046689bd5
AdishwarS <Adishwar.Suresh@infinite.com>
2025-07-21 02:55:18 -0500

    Add src/main/java/reactfrontendapp/Application.java

---
+++ src/main/java/reactfrontendapp/Application.java
@@ -0,0 +1,11 @@
+package reactfrontendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactfrontendappApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReactfrontendappApplication.class, args);
+    }
+}
